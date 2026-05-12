e218b0072a7463692db7781285113f7973d10e8c
firstrepo1 <shaikhqasim166@gmail.com>
2026-05-12 23:10:21 +0500

    Separate Button For ColorSwitcher

--- src/component/ColorSwitcherButton.jsx
+++ src/component/ColorSwitcherButton.jsx
@@ -3,18 +3,20 @@ const ColorSwitcherButton = ({ bgcolor, textColor, onClick, text }) => {
   return (
     <div>
       <button className="
-                px-5 py-2 m-2 rounded-xl font-medium
+                px-4 py-2 m-2 rounded-xl 
                 shadow-md
                 transition-all duration-300 ease-in-out
                 hover:scale-110
                 hover:shadow-xl
                 hover:-translate-y-1
                 active:scale-95  
-                cursor-pointer"
+                cursor-pointer
+                "
                 style={{backgroundColor : bgcolor , color: textColor} }
                  onClick={onClick}
                 >
-                        {text}
+                        {text + " Btn"}
+
                 </button>
    
    
